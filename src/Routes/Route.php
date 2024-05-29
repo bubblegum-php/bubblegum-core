72d4a0a700eb4eceaf7409fb3b2904863d557307
Oleksii Fedorenko <lankrf@gmail.com>
2024-05-29 17:17:06 +0300

    Add Route logic

--- src/Routes/Route.php
+++ src/Routes/Route.php
@@ -4,11 +4,35 @@ namespace Bubblegum\Routes;
 
 class Route
 {
-    /**
-     * @return string
-     */
-    static function getInfo(): string
+    protected static array $routes = [
+        'GET' => array(),
+        'POST' => array(),
+    ];
+
+    protected static array $defaultMethods = [
+        'GET' => 'index',
+        'POST' => 'store',
+    ];
+
+    public static function get(string $url, $controller): RouteConfig
     {
-        return 'Bubblegum-routes | version: alpha.';
+        return self::route('GET', $url, $controller);
+    }
+
+    public static function post(string $url, $controller): RouteConfig
+    {
+        return self::route('POST', $url, $controller);
+    }
+
+    public static function route(string $httpMethod, string $url, $controller): RouteConfig
+    {
+        $routeConfig = new RouteConfig($url, $controller, self::$defaultMethods[$httpMethod]);
+        self::$routes[$httpMethod][] = $routeConfig;
+        return $routeConfig;
+    }
+
+    public static function getRoutes(): array
+    {
+        return self::$routes;
     }
 }
