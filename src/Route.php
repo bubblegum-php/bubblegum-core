07594dfcbe9b123e73bb79c0271dda489509abe6
Oleksii Fedorenko <lankrf@gmail.com>
2024-05-29 13:13:19 +0300

    Starting routes module

---
+++ src/Route.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bubblegum\Routes;
+
+class Route
+{
+    /**
+     * @return string
+     */
+    static function getInfo(): string
+    {
+        return 'Bubblegum-routes | version: alpha.';
+    }
+}
