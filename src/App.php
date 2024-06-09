9b94432ceda6df76fe81b29a25b2399a15efb18c
Oleksii Fedorenko <lankrf@gmail.com>
2024-06-09 13:27:28 +0300

    Add error handling in app

--- src/App.php
+++ src/App.php
@@ -3,17 +3,18 @@
 namespace Bubblegum;
 
 use Bubblegum\Routes\RouteJunction;
+use Exception;
 
 class App
 {
     public static function run(): void
     {
-        $routeConfig = RouteJunction::getRouteConfig();
-        if (!$routeConfig) {
-            echo '<h1>404</h1>';
-            return;
+        try {
+            $routeConfig = RouteJunction::getRouteConfig();
+            $request = new Request();
+            echo $routeConfig->getRoutedComponent()->content($request, $routeConfig->getData());
+        } catch (Exception $ex) {
+            echo "<h1>Error {$ex->getCode()}</h1><pre>{$ex->getMessage()}</pre>";
         }
-        $request = new Request([]);
-        echo $routeConfig->getRoutedComponent()->referToDestination($request);
     }
 }
