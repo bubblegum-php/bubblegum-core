82be21a510b90c77b32a76d5f52fc2984e6e641e
Oleksii Fedorenko <lankrf@gmail.com>
2024-09-24 17:29:23 +0300

    Add raw data to request

--- src/Request.php
+++ src/Request.php
@@ -4,15 +4,40 @@ namespace Bubblegum;
 
 class Request
 {
+    protected array $rowData = [];
     public function __construct()
-    { }
-
-    public function all() {
-        return self::getRequestData();
+    {
+        if ($_SERVER['HTTP_CONTENT_TYPE'] === 'application/json') {
+            $this->rowData = json_decode(file_get_contents('php://input'), true);
+        }
     }
 
-    protected static function getRequestData(): array
+    public function all() {
+        return static::requestData() + $this->rowData;
+    }
+
+    public function rawData(): array
+    {
+        return $this->rowData;
+    }
+
+    public static function requestData(): array
     {
         return $_REQUEST;
+    }
+
+    public static function cookies(): array
+    {
+        return $_COOKIE;
+    }
+
+    public static function getData(): array
+    {
+        return $_GET;
+    }
+
+    public static function postData(): array
+    {
+        return $_POST;
     }
 }
